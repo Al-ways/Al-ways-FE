d08039c8fa04e8c9c14268c4ebcbb748d4440b0e
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-09-16 12:08:06 +0900

    feat: '공용 컬러 템플릿 추가'
    공용 컬러 템플릿 추가했습니다.

---
+++ src/share/colors.ts
@@ -0,0 +1,21 @@
+const COLORS = {
+  black: '#000000',
+  white: '#fffff',
+  main: '#FFBAAB', // profile div, button 등 가장 자주 쓰이는 color.
+  background: 'FFCAB1', // 배경 등의 color. main color와 조금 차이 있음.
+
+  dark_gray: '#3D3D3D;', // 기본 div 배경 색.
+  gray: '#D9D9D9;', // 회색을 써야하는데 배경 색과 차이를 두어야 할 때.
+
+  // 생성되는 card들에 뿌려줄 배경 색.
+  sky: '7C96AB',
+  navy: '273348',
+  light_orange: 'EDC6B1',
+  apricot: 'FF9881',
+  olive_green: 'BFCCB5',
+  pink: 'EF8790',
+  light_pink: 'F49DA9',
+  powder_pink: 'FFC9C9',
+  mustard: 'CAC888',
+  red_bean: '834E67',
+};
