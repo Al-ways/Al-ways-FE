a8377f0b2d707df7ca243621c1ce0a8632a83653
배성완 <baesee@kakao.com>
2023-09-20 18:34:03 +0900

    design: '카테고리 select option'
    OptionButton.tsx: list(string[])값을 받아서 option 값을 줍니다.
    
    Related to: #11

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,9 +1,16 @@
+import OptionButton from '../components/atom/OptionButton';
+import styled from '@emotion/styled';
 const List = () => {
+  const optionList: string[] = ['카테고리', '이자카야', 'pup'];
+
   return (
-    <>
-      <h1>this is List page</h1>
-    </>
+    <Container>
+      <OptionButton list={optionList} />
+    </Container>
   );
 };
-
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 export default List;
