7ef1522df70ce81a792d1e3a01295a6193f24962
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-09-20 17:54:56 +0900

    feat: 'Text atom 수정'
    Text atom props (margin-top, postion) 추가 및 TypeSciript 코드 간소화
    
    Relate to: #10'

--- src/components/atom/Text.tsx
+++ src/components/atom/Text.tsx
@@ -1,14 +1,18 @@
 import styled from '@emotion/styled';
+import { COLORS } from '../../share/colors';
 interface TextProps {
   width?: string;
   height?: string;
   color?: string;
   align?: string;
   vertical?: string;
-  txt: string | number;
+  txt?: string | number;
   fonts?: string;
   fontw?: string;
   mb?: string;
+  mt?: string;
+  bottom?: string;
+  position?: string;
   br?: string;
 }
 
@@ -22,6 +26,9 @@ const Text = ({
   fonts,
   fontw,
   mb,
+  mt,
+  bottom,
+  position,
   br,
 }: TextProps) => {
   return (
@@ -34,31 +41,27 @@ const Text = ({
       fonts={fonts}
       fontw={fontw}
       mb={mb}
+      mt={mt}
+      bottom={bottom}
+      position={position}
       br={br}
     >
       {txt}
     </TextContainer>
   );
 };
-const TextContainer = styled.div<{
-  width?: string;
-  height?: string;
-  color?: string;
-  align?: string;
-  vertical?: string;
-  fonts?: string;
-  fontw?: string;
-  mb?: string;
-  br?: string;
-}>`
+const TextContainer = styled.div<TextProps>`
   width: ${(props) => props.width + 'px' || 'auto'};
   height: ${(props) => props.height + 'px' || 'auto'};
   text-align: ${(props) => props.align || 'start'};
-  color: ${(props) => props.color || '#ffffff'};
+  color: ${(props) => props.color || COLORS.white};
   font-size: ${(props) => props.fonts + 'px' || '16px'};
   line-height: ${(props) => props.height + 'px' || 'auto'};
   font-weight: ${(props) => props.fontw || 'normal'};
   margin-bottom: ${(props) => props.mb + 'px' || '0px'};
+  margin-top: ${(props) => props.mt + 'px' || '0px'};
+  bottom: ${(props) => props.bottom + 'px' || '0px'};
+  position: ${(props) => props.position || 'auto'};
   border-radius: ${(props) => props.br + 'px' || '0px'};
 `;
 export default Text;
