56a66aa8f70898cb88a9960152f17df9295f173f
배성완 <baesee@kakao.com>
2023-09-22 19:23:46 +0900

    design 'login page 구현'
    Related to: #22

--- src/components/organism/SocialLoginOranism.tsx
+++ src/components/organism/SocialLoginOranism.tsx
@@ -1,36 +1,18 @@
 import styled from '@emotion/styled';
 import SocialLoginButtonMolecule from '../molecule/SocialLoginButtonMolecule';
-import Text from '../atom/Text';
 const SocialLoginOranism = () => {
   return (
     <Container>
-      <Text
-        txt={'로그인 해주세요.'}
-        width={'440'}
-        height={'150'}
-        align={'center'}
-        fonts={'40'}
-      />
-      <LoginBox>
-        <SocialLoginButtonMolecule name={'kakao'} />
-        <SocialLoginButtonMolecule name={'naver'} />
-        <SocialLoginButtonMolecule name={'google'} />
-      </LoginBox>
+      <SocialLoginButtonMolecule name={'kakao'} />
+      <SocialLoginButtonMolecule name={'naver'} />
+      <SocialLoginButtonMolecule name={'google'} />
     </Container>
   );
 };
 const Container = styled.div`
-  width: 440px;
+  width: 480px;
   height: 300px;
-  border-radius: 12px;
-  background-color: #ae73b8;
-`;
-const LoginBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  height: 150px;
+  margin: 0 auto;
 `;
 
 export default SocialLoginOranism;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import styled from '@emotion/styled';
+import SocialLoginOranism from '../components/organism/SocialLoginOranism';
+import bgLogin from '../assets/login/bgLogin.png';
+import HeaderMolecule from '../components/molecule/HeaderMolecule';
+import GoBackMolecule from '../components/molecule/GoBackMolecule';
+import { useParams } from 'react-router-dom';
 const Login = () => {
-  return <div>Login</div>;
-};
+  const ref = useParams();
+  console.log(ref.id);
 
+  return (
+    <Container>
+      <HeaderMolecule />
+      <GoBackMolecule />
+      <SocialLoginOranism />
+    </Container>
+  );
+};
+const Container = styled.div`
+  width: 480px;
+  height: 909px;
+
+  background-image: url(${bgLogin});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 export default Login;
