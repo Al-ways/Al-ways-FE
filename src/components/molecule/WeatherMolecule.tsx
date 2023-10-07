a682204db68add9c1949d3e643b1eb385ad73d44
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-10-08 06:40:40 +0900

    feat: '현재 위치한 지역 및 기온, 날씨 렌더링 구현' >> >> 기온: 화씨 -> 섭씨 설정 변경 >> 날씨: 기존 API의 12개 종류 날씨를 6개로 축약 및 한국어 변환 >> 현재 위치한 지역은 서울시 25개 구의 영문명을 차후 한국어 변환해 출력할 예정 >> >> Related to: #40 >>

--- src/components/molecule/WeatherMolecule.tsx
+++ src/components/molecule/WeatherMolecule.tsx
@@ -3,28 +3,60 @@ import Text from '../atom/Text';
 import { COLORS } from '../../share/colors';
 import Weather from '../atom/Weather';
 
-const WeatherMolecule = () => {
+interface WeatherProps {
+  name: string;
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+  }[];
+}
+
+const WeatherMolecule = ({ weather }: { weather: WeatherProps }) => {
+  // console.log(weather);
+
+  // 날씨 번역
+  const weatherTranslation: { [key: string]: string } = {
+    'sky is clear': '맑음',
+    'few clouds': '구름',
+    'scattered clouds': '구름',
+    'broken clouds': '구름',
+    'overcast clouds': '구름',
+    'shower rain': '비',
+    'light rain': '비',
+    'moderate rain': '비',
+    Rain: '비',
+    Thunderstorm: '천둥번개',
+    snow: '눈',
+    mist: '안개',
+  };
+
+  const translatedWeather =
+    weatherTranslation[weather.weather[0].description] ||
+    weather.weather[0].description;
+
   return (
-    <>
-      <WheatherBanner>
-        <Text
-          width={'400'}
-          height={'50'}
-          fonts={'45'}
-          mt={'20'}
-          txt={'비 오는 날엔?'}
-        />
-        <Text
-          width={'400'}
-          height={'16'}
-          fonts={'16'}
-          txt={'강남구, 22°C, 비'}
-          bottom={'20'}
-          position={'absolute'}
-        />
-        <Weather />
-      </WheatherBanner>
-    </>
+    <WheatherBanner>
+      <Text
+        width={'400'}
+        height={'50'}
+        fonts={'45'}
+        mt={'20'}
+        txt={'비 오는 날엔?'}
+      />
+      <Text
+        width={'400'}
+        height={'16'}
+        fonts={'16'}
+        txt={`${weather.name}, ${Math.floor(
+          weather.main.temp,
+        )}°C, ${translatedWeather}`}
+        bottom={'20'}
+        position={'absolute'}
+      />
+      <Weather />
+    </WheatherBanner>
   );
 };
 
--- src/components/organism/WeatherOrganism.tsx
+++ src/components/organism/WeatherOrganism.tsx
@@ -19,7 +19,7 @@ const WeatherOrganism = () => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=576217254852d28ff29e275ca843c5fe&units=metric`;
     let response = await fetch(url);
     let data = await response.json();
-    // console.log(data);
+    console.log(data);
     setWeather(data);
   };
 
@@ -28,9 +28,7 @@ const WeatherOrganism = () => {
   }, []);
 
   return (
-    <Container>
-      <WeatherMolecule weather={weather} />
-    </Container>
+    <Container>{weather && <WeatherMolecule weather={weather} />}</Container>
   );
 };
 
