53c65d191acca1975612744ba6540ca6b4730251
배성완 <baesee@kakao.com>
2023-11-10 16:46:29 +0900

    design : loading page ui
    Relate to : #70

--- src/pages/ExaminationResultLoading.tsx
+++ src/pages/ExaminationResultLoading.tsx
@@ -4,23 +4,34 @@ import { examinationResult } from '../api/examination';
 import { useDispatch } from 'react-redux';
 import { pushResult } from '../redux/reducers/examinationResultArr';
 import { useNavigate } from 'react-router-dom';
-
+import styled from '@emotion/styled';
+import loading from '../assets/loading.gif';
+import Text from '../components/atom/Text';
+interface answerArrStateType {
+  examinatonAnswerArr: {
+    answerArr: Array<number>;
+  };
+}
+interface resultArrStateType {
+  examinationResultArr: {
+    data: Array<{ id: string; name: string; description: string; img: string }>;
+  };
+}
 function ExaminationResultLoading() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const answerArr = useSelector(
-    (state: any) => state.examinatonAnswerArr.answerArr,
+    (state: answerArrStateType) => state.examinatonAnswerArr.answerArr,
   );
 
   const resultArr = useSelector(
-    (state: any) => state.examinationResultArr.data,
+    (state: resultArrStateType) => state.examinationResultArr.data,
   );
-  console.log(answerArr, resultArr);
 
   useEffect(() => {
     const result = examinationResult(answerArr);
     dispatch(pushResult(result));
-  }, []);
+  }, [answerArr, dispatch]);
 
   useEffect(() => {
     if (resultArr[0]?.name) {
@@ -32,7 +43,20 @@ function ExaminationResultLoading() {
       };
     }
   }, [resultArr, navigate]);
-  return <div>loading....</div>;
+  return (
+    <Container>
+      <img src={loading} />
+      <Text txt={'로딩중...'} fontw={'bold'} fonts={'24'} />
+    </Container>
+  );
 }
 
 export default ExaminationResultLoading;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60dvh;
+`;
